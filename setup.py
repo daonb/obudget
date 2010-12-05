9ee9a1f35d92c0395123f9ed6b3dce7e4c57a400
בני <daonb@levavit.local>
2010-12-05 12:20:15 +0200

    initial commit - just the buildout script and some packages

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+install_requires = [
+    'South',
+    'django-piston',
+    'django-extensions',
+    'python-memcached',
+    ]
+setup(
+        name = "obudget",
+        version = "0.1",
+        url = 'http://github.com/hasadna/obudget',
+        description = "Bringing transperancy to the Israeli Knesset",
+        author = 'Benny Daon, Imri Goldberg and others',
+        packages = find_packages('src'),
+        package_dir = {'': 'src'},
+        install_requires = install_requires,
+        classifiers=['Development Status :: 2 - Pre-Alpha',
+                 'Environment :: Web Environment',
+                 'Framework :: Django',
+                 'License :: OSI Approved :: BSD License',
+                 'Natural Language :: Hebrew',
+                 'Operating System :: OS Independent',
+                 'Programming Language :: Python',
+                 'Programming Language :: JavaScript'],
+)
+
+
