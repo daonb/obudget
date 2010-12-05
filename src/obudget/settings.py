9c24aefa9ae542bb0a9123668cb42974dc40afbc
בני <daonb@levavit.local>
2010-12-05 15:03:57 +0200

    adding missing app

--- src/obudget/settings.py
+++ src/obudget/settings.py
@@ -91,6 +91,7 @@ INSTALLED_APPS = (
     # 'django.contrib.messages',
     'django.contrib.admin',
     'south',
+    'django_extensions',
     'obudget.budget_lines',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
