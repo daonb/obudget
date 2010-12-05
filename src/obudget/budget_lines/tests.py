d3cf56600786c85e81bac0c0e36bf25baa0851c5
daonb <bennydaon@gmail.com>
2010-12-05 18:12:44 +0200

    basic piston handler is here

--- src/obudget/budget_lines/tests.py
+++ src/obudget/budget_lines/tests.py
@@ -6,18 +6,15 @@ Replace these with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from django.core.urlresolvers import reverse
+from models import *
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
+class APITest(TestCase):
+    def setUp(self):
+    	self.line1 = BudgetLine.objects.create(title="line 1", budget_id="0001",
+		amount_allocated=2, amount_revised=20, amount_used=18, year=1970)
 
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
-
->>> 1 + 1 == 2
-True
-"""}
-
+    def test_basic_line(self):
+	res = self.client.get(reverse('budget-line-handler', kwargs={'id':'0001'}))
+        self.assertEqual(res.status_code, 200)
+	# TODO: test content
--- src/obudget/urls.py
+++ src/obudget/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.defaults import *
 
 from obudget import views
+from obudget.budget_lines.handlers import budget_line_handler
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -13,8 +14,10 @@ urlpatterns = patterns('',
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    (r'^$', views.main),
+    # (r'^$', views.main),
 
+    # sould be the last line
+    url(r'^(?P<id>[0-9]+)$', budget_line_handler, name='budget-line-handler'),
     # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
 )
