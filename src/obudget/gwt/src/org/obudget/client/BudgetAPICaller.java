bc73e1346a63419cf3a2c1ec9dd87dc0735afebf
daonb <bennydaon@gmail.com>
2010-12-13 15:20:09 +0200

    supporting all browsers

--- src/obudget/gwt/src/org/obudget/client/BudgetAPICaller.java
+++ src/obudget/gwt/src/org/obudget/client/BudgetAPICaller.java
@@ -12,8 +12,8 @@ class BudgetAPICaller extends JsonpRequestBuilder {
 
 	public BudgetAPICaller() {
 		url = new UrlBuilder();
-		url.setHost("127.0.0.1");
-		url.setPort(8000);
+		url.setHost("budget.yeda.us");
+		url.setPort(80);
 		url.setPath("00");
 	}
 	
