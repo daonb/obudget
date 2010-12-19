d34980dddceebb246c501e53392c6f7b55425839
Adam Kariv <adam.kariv@gmail.com>
2010-12-19 09:56:13 +0200

    fix DB data and csv-to-db command

--- src/obudget/budget_lines/management/commands/budget_lines_csv_to_db.py
+++ src/obudget/budget_lines/management/commands/budget_lines_csv_to_db.py
@@ -17,39 +17,44 @@ class Command(BaseCommand):
         BudgetLine.objects.all().delete()
                 
         print 'Loading raw rows'
-        i = 0
-        for d in reader:              
+        x = set()
+        k = 0
+        for d in reader:
+            key = d['budget_id'], d['year']
+            if key in x:
+                continue
+            x.add(key)         
             BudgetLine( title = d['title'].decode('utf8'),
                         budget_id = d['budget_id'],
                         amount_allocated = int(d['allocated']),
                         amount_revised = int(d['revised']),
                         amount_used = int(d['used']),
                         year = int(d['year']),
+                        budget_id_len = len(d['budget_id']),
                         ).save()
-            i+=1
-            if i % 1000 == 0:
-                print i
+            k+=1
+            if k % 1000 == 0:
+                print k
 
         
         # Update internal relationships in the DB
         print 'Internal relationships'
-        i = 0
+        k = 0
         for line in BudgetLine.objects.all():
-            i+=1
-            if i % 1000 == 0:
-                print i
+            k+=1
+            if k % 1000 == 0:
+                print k
 
             if line.budget_id == None or len(line.budget_id) == 2:        
                 continue
             
             for i in range(2,len(line.budget_id),2):
-                try:
+                parents = BudgetLine.objects.filter( year = line.year, budget_id = line.budget_id[:-i] ).count()
+                if parents > 0:
                     parent = BudgetLine.objects.get( year = line.year, budget_id = line.budget_id[:-i] )
                     line.containing_line = parent
                     line.save()
                     break
-                except:
-                    pass
             
         
         
