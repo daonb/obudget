7a4cee3397cb992caf0bf00cf818291a7f0046a9
Adam Kariv <adam.kariv@gmail.com>
2011-01-25 20:16:18 +0200

    housecleaning

--- data/make_master_jsons.py
+++ data/make_master_jsons.py
@@ -1,3 +1,5 @@
+#### encoding: utf8
+
 import json
 filenames = [ "financial_report/2000-2008/out.json",
               "financial_report/2009/out.json",
@@ -25,11 +27,33 @@ for f in filenames:
         try:
             rec = json.loads(line)
             if rec.get('title') == None: continue
+            if rec['code'].startswith('0000'):
+                continue
             key = "%s|%10s" % (rec['year'],rec['code'])
             keys.add(key)
             alldata.setdefault(key,{}).update(rec)
         except Exception,e:
             print "error %s in line %r" % (e,line)
+    
+for year in range(1992,2013):
+    totalkey = '%s|%8s' % (year,"00")
+    if totalkey in keys:
+        continue
+    totalrec = { 'title'     : u'המדינה',
+                 'year'      : year,
+                 'budget_id' : '00',
+                 }
+    for key in keys:
+        if key.startswith(totalkey):
+            rec = alldata[key]
+            for k,v in rec.iteritems():
+                if type(v) == int:
+                    totalrec[k] = totalrec.setdefault(k,0) + v
+
+    print 'total:',year,totalrec
+                     
+    alldata.setdefault(totalkey,{}).update(totalrec)
+    
         
 keys = list(keys)
 keys.sort()
