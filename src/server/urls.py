56d24fd5f03df2a4a946f5df139c87f3e0db393e
Benny Daon <bennydaon@gmail.com>
2011-03-13 22:45:21 +0200

    adding caching

--- src/server/urls.py
+++ src/server/urls.py
@@ -1,11 +1,13 @@
+
 import os
 
 from django.conf.urls.defaults import *
+from django.views.decorators.cache import cache_page
 
 from budget_lines.handlers import budget_line_handler
 
 urlpatterns = patterns('',
-    url(r'^(?P<id>[0-9]+)$', budget_line_handler, name='budget-line-handler'),
+    url(r'^(?P<id>[0-9]+)$', cache_page(budget_line_handler, 30*60), name='budget-line-handler'),
     (r'^gwt/(?P<path>.*)$', 'django.views.static.serve', {'document_root':
                                                           os.path.realpath('src/client/war/')}),
 
